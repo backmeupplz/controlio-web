9f2b0c48f6a86745f66964882565c72c003b3602
Михаил Ерохов <kingmixaileroxov@gmail.com>
2017-02-21 22:46:37 +0300

    add svg module

---
+++ src/+app/Svg/Svg.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { SvgIconComponent }   from './SvgIcon/SvgIcon.component';
+
+@NgModule({
+      imports: [],
+      exports: [SvgIconComponent],
+      declarations: [SvgIconComponent],
+      providers: [],
+})
+export class SvgModule {}
